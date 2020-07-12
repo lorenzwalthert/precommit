4205d2c4c4c7e08289734c9ae3df0de5274c17c6
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-07-12 16:23:43 +0200

    fiddle again

--- tests/testthat/test-all.R
+++ tests/testthat/test-all.R
@@ -27,7 +27,7 @@ run_test("style-files",
 run_test("style-files",
   file_name = "style-files-cmd",
   suffix = "-success.R",
-  cmd_args = c("--style_pkg=styler", "--style_transformers='tidyverse_style(scope = \"none\")'")
+  cmd_args = c("--style_pkg=styler", "'--style_transformers=tidyverse_style(scope = \"none\")'")
 )
 
 # fail
