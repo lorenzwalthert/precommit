a2411bca4e7bdb73f02fc1001cd3c9d1899f6ed7
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-07-14 00:17:09 +0200

    fix arg

--- tests/testthat/test-all.R
+++ tests/testthat/test-all.R
@@ -1,17 +1,23 @@
 run_test("style-files",
          file_name = "style-files-cmd",
          suffix = "-success.R",
-         cmd_args = c("--style_pkg=styler", "'--style_transformers=tidyverse_style(scope = \"none\")'")
+         cmd_args = c("--style_pkg=styler", "'--arg=tidyverse_style(scope = \"none\")'")
 )
 
 run_test("style-files",
          file_name = "style-files-cmd",
          suffix = "-success.R",
-         cmd_args = c("--style_pkg=styler", "--style_transformers='tidyverse_style(scope = \"none\")'")
+         cmd_args = c("--style_pkg=styler", "--arg='tidyverse_style(scope = \"none\")'")
 )
 
 run_test("style-files",
          file_name = "style-files-cmd",
          suffix = "-success.R",
-         cmd_args = c("--style_pkg=styler", "--style_transformers='tidyverse_style(scope= \"none\")'")
+         cmd_args = c("--style_pkg=styler", "--arg='tidyverse_style(scope= \"none\")'")
+)
+
+run_test("style-files",
+         file_name = "style-files-cmd",
+         suffix = "-success.R",
+         cmd_args = c("--style_pkg=styler", "--arg='tidyverse_style(scope= \"none\")'")
 )
