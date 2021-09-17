f76089422bedefd1394a88dbc99a7abdfac33437
Lorenz Walthert <lorenz.walthert@icloud.com>
2021-09-17 13:35:18 +0200

    drop httr and co for lintr

--- inst/renv-update.R
+++ inst/renv-update.R
@@ -3,7 +3,11 @@ hook_deps <- function(root) {
   out <- renv::dependencies("inst/hooks/exported/")$Package
   desc <- desc::desc()
   deps <- desc$get_deps()
-  dont <- c("yaml", "usethis", "withr", "rstudioapi", "precommit")
+  dont <- c(
+    "yaml", "usethis", "withr", "rstudioapi", "precommit",
+    "httr" # lintr -> httr  -> curl -> libcurl, but seems to give no erorr on
+    # loading lintr, plus https://github.com/jimhester/lintr/issues/861
+  )
   out <- setdiff(c(unique(c(out, deps[deps$type == "Imports", ]$package))), dont)
   return(out)
 }
