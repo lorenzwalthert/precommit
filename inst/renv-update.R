21fcbc0dd00e9358431d837407dd9fcdbfe6a9aa
Lorenz Walthert <lorenz.walthert@icloud.com>
2021-09-19 20:34:33 +0200

    also check recursive dependenices with appropriate renv function

--- inst/renv-update.R
+++ inst/renv-update.R
@@ -10,6 +10,7 @@ hook_deps <- function(root) {
   )
   out <- c(out, "roxygen2", "spelling", "styler", "pkgload", "lintr", "knitr", "git2r", "digest", "desc")
   out <- setdiff(c(unique(c(out, deps[deps$type == "Imports", ]$package))), dont)
+  out <- names(renv:::renv_package_dependencies(out))
   return(out)
 }
 
