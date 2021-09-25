53af04fca35ccae938bd947c2aa15cf09bc00ffa
Lorenz Walthert <lorenz.walthert@icloud.com>
2021-09-25 19:05:27 +0200

    move advanced bash to R

--- inst/hooks/exported/readme-rmd-rendered.R
+++ inst/hooks/exported/readme-rmd-rendered.R
@@ -4,13 +4,13 @@ if (file.exists("README.Rmd") & file.exists("README.md")) {
     rlang::abort("README.md is out of date; please re-knit README.Rmd.")
   }
 
-  num_readmes <- as.integer(system2(
-    "git",
-    c("diff", "--cached", "--name-only", "|", "grep", "-Eic", "'^README\\.[R]?md$'"),
+  file_names_staged <- system2(
+    "git", c("diff --cached --name-only"),
     stdout = TRUE
-  ))
+  )
+  num_readmes <- length(grepl("^README\\.[R]?md$", file_names_staged))
 
-  if (num_readmes < 2) {
+  if (num_readmes == 1) {
     rlang::abort("README.Rmd and README.md should be both staged.")
   }
 }
