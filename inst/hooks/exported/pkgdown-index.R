fae18fff61bf5c71e5a1a4fa72679831a10e1ac1
gravesti <isaac.gravestock@roche.com>
2022-05-05 16:18:02 +0200

    simple pkgdown check

---
+++ inst/hooks/exported/pkgdown-index.R
@@ -0,0 +1,17 @@
+#!/usr/bin/env Rscript
+
+if (!require(pkgdown)) stop("{pkgdown} could not be loaded.")
+if (!require(mockery)) stop("{mockery} could not be loaded.")
+
+stub_render_page <- function(pkg, name, data, ...) force(data)
+stub(build_reference_index, "render_page", stub_render_page)
+stub(build_articles_index, "render_page", stub_render_page)
+
+tryCatch(
+  {
+    build_reference_index()
+    build_articles_index()
+  },
+  error = function(e) e,
+  warning = function(w) stop(w)
+)
