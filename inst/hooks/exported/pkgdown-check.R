ebcc0542098dd8f7fe5d30572203147b3e43d84a
Lorenz Walthert <lorenz.walthert@icloud.com>
2022-05-07 14:09:06 +0200

    format

--- inst/hooks/exported/pkgdown-check.R
+++ inst/hooks/exported/pkgdown-check.R
@@ -1,9 +1,15 @@
 #!/usr/bin/env Rscript
 
-if (!require(pkgdown)) stop("{pkgdown} could not be loaded.")
-if (!require(mockery)) stop("{mockery} could not be loaded.")
+if (!require(pkgdown)) {
+  stop("{pkgdown} could not be loaded.")
+}
+if (!require(mockery)) {
+  stop("{mockery} could not be loaded.")
+}
 
-stub_render_page <- function(pkg, name, data, ...) force(data)
+stub_render_page <- function(pkg, name, data, ...) {
+  force(data)
+}
 stub(build_reference_index, "render_page", stub_render_page)
 stub(build_articles_index, "render_page", stub_render_page)
 
