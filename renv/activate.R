80a688d201f66a4a7a1afc7bbb541866b5003e3a
Lorenz Walthert <lorenz.walthert@icloud.com>
2022-11-13 16:34:26 +0100

    update renv package

--- renv/activate.R
+++ renv/activate.R
@@ -2,7 +2,7 @@
 local({
 
   # the requested version of renv
-  version <- "0.12.5"
+  version <- "0.16.0"
 
   # the project directory
   project <- getwd()
@@ -94,11 +94,8 @@ local({
       return(repos)
   
     # if we're testing, re-use the test repositories
-    if (renv_bootstrap_tests_running()) {
-      repos <- getOption("renv.tests.repos")
-      if (!is.null(repos))
-        return(repos)
-    }
+    if (renv_bootstrap_tests_running())
+      return(getOption("renv.tests.repos"))
   
     # retrieve current repos
     repos <- getOption("repos")
@@ -662,8 +659,8 @@ local({
     if (version == loadedversion)
       return(TRUE)
   
-    # assume four-component versions are from GitHub;
-    # three-component versions are from CRAN
+    # assume four-component versions are from GitHub; three-component
+    # versions are from CRAN
     components <- strsplit(loadedversion, "[.-]")[[1]]
     remote <- if (length(components) == 4L)
       paste("rstudio/renv", loadedversion, sep = "@")
@@ -845,29 +842,11 @@ local({
   
   renv_json_read <- function(file = NULL, text = NULL) {
   
-    jlerr <- NULL
-  
     # if jsonlite is loaded, use that instead
-    if ("jsonlite" %in% loadedNamespaces()) {
-  
-      json <- catch(renv_json_read_jsonlite(file, text))
-      if (!inherits(json, "error"))
-        return(json)
-  
-      jlerr <- json
-  
-    }
-  
-    # otherwise, fall back to the default JSON reader
-    json <- catch(renv_json_read_default(file, text))
-    if (!inherits(json, "error"))
-      return(json)
-  
-    # report an error
-    if (!is.null(jlerr))
-      stop(jlerr)
+    if ("jsonlite" %in% loadedNamespaces())
+      renv_json_read_jsonlite(file, text)
     else
-      stop(json)
+      renv_json_read_default(file, text)
   
   }
   
