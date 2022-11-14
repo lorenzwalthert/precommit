345e95f33721459767296a493a8002857f793824
Lorenz Walthert <lorenz.walthert@icloud.com>
2022-11-14 17:41:52 +0100

    go for the easy way

--- inst/hooks/exported/pkgdown.R
+++ inst/hooks/exported/pkgdown.R
@@ -12,26 +12,7 @@ if (is.null(pkgdown:::pkgdown_config_path("."))) {
 if (!require(pkgdown, quietly = TRUE)) {
   stop("{pkgdown} could not be loaded, please install it.")
 }
-if (!require(mockery, quietly = TRUE)) {
-  stop("{mockery} could not be loaded, please install it.")
+if (packageVersion("pkgdown") < package_version("2.0.4")) {
+  rlang::abort("You need at least version 2.0.4 of {pkgdown} to run this hook.")
 }
-
-stub_render_page <- function(pkg, name, data, ...) {
-  force(data)
-}
-stub(build_reference_index, "render_page", stub_render_page)
-stub(build_articles_index, "render_page", stub_render_page)
-config <- yaml::read_yaml(pkgdown:::pkgdown_config_path("."))
-
-tryCatch(
-  {
-    if ("reference" %in% names(config)) {
-      build_reference_index()
-    }
-    if ("articles" %in% names(config)) {
-      build_articles_index()
-    }
-  },
-  error = function(e) stop(e),
-  warning = function(w) stop(w)
-)
+check_pkgdown()
