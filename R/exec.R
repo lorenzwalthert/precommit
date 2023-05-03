b547c85e7831ab475738684c9a5ffb47d4ef4054
Lorenz Walthert <lorenzwalthert@users.noreply.github.com>
2023-05-03 19:21:51 +0200

    in windows, base is just r-precommit/, not r-precommit/bin/

--- R/exec.R
+++ R/exec.R
@@ -207,15 +207,15 @@ path_derive_precommit_exec_conda_impl <- function(conda_env) {
     {
       ls <- reticulate::conda_list()
       path_reticulate <- fs::path_dir(ls[ls$name == conda_env, "python"][1])
-      cat("path_reticulate:", path_reticulate)
+      cat("path_reticulate:", path_reticulate, "\n")
       derived <- fs::path(
-        # if windows, remove bin/ and use just base directory
-        ifelse(is_windows(), fs::path_dir(path_reticulate), path_reticulate),
+        # if windows, this is r-precommit, otherwise, it's r-precommit/env and use just base directory
+        path_reticulate,
         # if windows, append Scripts/
         ifelse(is_windows(), "Scripts", ""),
         precommit_executable_file()
       )
-      cat("derived", derived)
+      cat("derived:", derived, "\n")
       unname(ifelse(file_exists(derived), derived, ""))
     },
     error = function(e) ""
