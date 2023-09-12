fa92971bc3e82181c2c419281880781a14d17e18
Tymoteusz Makowski <tymoteusz.makowski@appsilon.com>
2023-09-12 11:39:58 +0200

    feat: add --no-update flag

--- inst/hooks/exported/spell-check.R
+++ inst/hooks/exported/spell-check.R
@@ -2,10 +2,11 @@
 
 "Spell check for files
 Usage:
-  spell-check [--lang=<language>] <files>...
+  spell-check [--lang=<language>] [--no-update] <files>...
 
 Options:
   --lang=<language> Passed to `spelling::spell_check_files()` [default: en_US]
+  --no-update       Don't update inst/WORDLIST with errors (new words)
 
 " -> doc
 
@@ -34,21 +35,26 @@ spelling_errors <- spelling::spell_check_files(
 if (nrow(spelling_errors) > 0) {
   cat("The following spelling errors were found:\n")
   print(spelling_errors)
-  ignore_df <- data.frame(
-    original = unique(c(ignore, spelling_errors$word))
-  )
-  ignore_df$lower <- tolower(ignore_df$original)
-  ignore_df <- ignore_df[order(ignore_df$lower), ]
-  ignore <- ignore_df$original[ignore_df$lower != ""] # drop blanks if any
-  writeLines(ignore, path_wordlist)
-  cat(
-    "All spelling errors found were copied to inst/WORDLIST assuming they were",
-    "not spelling errors and will be ignored in the future. Please ",
-    "review the above list and for each word that is an actual typo:\n",
-    "- fix it in the source code.\n",
-    "- remove it again manually from inst/WORDLIST to make sure it's not\n",
-    "  ignored in the future.\n",
-    "Then, try committing again.\n"
-  )
+  if (isTRUE(arguments$no_update)) {
+    cat("Hint: you can enable an automatic updating of WORDLIST with errors (new words) by removing the --no-update flag.\n")
+  } else {
+    ignore_df <- data.frame(
+      original = unique(c(ignore, spelling_errors$word))
+    )
+    ignore_df$lower <- tolower(ignore_df$original)
+    ignore_df <- ignore_df[order(ignore_df$lower), ]
+    ignore <- ignore_df$original[ignore_df$lower != ""] # drop blanks if any
+    writeLines(ignore, path_wordlist)
+    cat(
+      "All spelling errors found were copied to inst/WORDLIST assuming they were",
+      "not spelling errors and will be ignored in the future. Please",
+      "review the above list and for each word that is an actual typo:\n",
+      "- fix it in the source code.\n",
+      "- remove it again manually from inst/WORDLIST to make sure it's not\n",
+      "  ignored in the future.\n",
+      "Then, try committing again.\n",
+      "Hint: you can disable this behavior by providing a --no-update flag.\n"
+    )
+  }
   stop("Spell check failed", call. = FALSE)
 }
