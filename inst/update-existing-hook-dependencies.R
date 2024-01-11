6786eb8174237712b0dff31cb50c004e29c43886
Lorenz Walthert <lorenzwalthert@users.noreply.github.com>
2024-01-11 21:23:58 +0100

    improve lockfile contents

--- inst/update-existing-hook-dependencies.R
+++ inst/update-existing-hook-dependencies.R
@@ -1,8 +1,9 @@
 source("renv/activate.R")
 
 options(
-  # repos = c(CRAN = "https://packagemanager.rstudio.com/all/latest"),
-  install.packages.compile.from.source = "never"
+  repos = c(RSPM = renv::lockfile_read()$R$Repositories$RSPM), # CRAN is not an option, always use PPM
+  install.packages.check.source = "no", # don't check if source packages are available
+  install.packages.compile.from.source = "never", # probably redundant with the above 'no': If source package is available, only use source if no code needs to be compiled (needs compilation flag on CRAN).
 )
 renv::install("jsonlite")
 renv_deps <- names(jsonlite::read_json("renv.lock")$Packages)
