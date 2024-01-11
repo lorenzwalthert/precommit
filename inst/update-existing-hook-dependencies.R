3d3cdcf71c20bd49f2b638c9d67a9be2e11f59c3
Lorenz Walthert <lorenzwalthert@users.noreply.github.com>
2024-01-11 21:41:29 +0100

    no comma
    [ci skip]

--- inst/update-existing-hook-dependencies.R
+++ inst/update-existing-hook-dependencies.R
@@ -1,11 +1,12 @@
+source("renv/activate.R")
+
 options(
   repos = c(RSPM = renv::lockfile_read()$R$Repositories$RSPM), # CRAN is not an option, always use PPM
   install.packages.check.source = "no", # don't check if source packages are available
-  install.packages.compile.from.source = "never", # probably redundant with the above 'no': If source package is available, only use source if no code needs to be compiled (needs compilation flag on CRAN).
+  install.packages.compile.from.source = "never" # probably redundant with the above 'no': If source package is available, only use source if no code needs to be compiled (needs compilation flag on CRAN).
 )
 renv::install("jsonlite")
 renv_deps <- names(jsonlite::read_json("renv.lock")$Packages)
-source("renv/activate.R")
 renv::load()
 renv::restore(prompt = FALSE)
 can_be_updated <- renv::update(setdiff(renv_deps, "renv"), prompt = FALSE, check = FALSE)
