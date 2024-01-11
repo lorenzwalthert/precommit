7fd41788fc87e03fb40877c645d12374aef24ce5
Lorenz Walthert <lorenzwalthert@users.noreply.github.com>
2024-01-11 22:37:07 +0100

    erase cran form lock file to ensure only rspm is used
    [ci skip]

--- inst/update-renv-prepare.R
+++ inst/update-renv-prepare.R
@@ -1,10 +1,4 @@
-# emulate renv::lockfile_read()$R$Repositories$RSPM, which is not available on previous version of renv
-
 options(
-  repos = c(RSPM = {
-    all_repos <- jsonlite::read_json("renv.lock")$R$Repositories
-    all_repos[sapply(all_repos, \(x) x$Name == "RSPM")][[1]]$URL
-  }), # CRAN is not an option, always use PPM
   install.packages.check.source = "no", # don't check if source packages are available
   install.packages.compile.from.source = "never" # probably redundant with the above 'no': If source package is available, only use source if no code needs to be compiled (needs compilation flag on CRAN).
 )
