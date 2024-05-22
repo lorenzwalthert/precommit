ea57e1aea27b2b4f8f26c32251be0cc78f0bc75d
Jack Kennedy <jack_kennedy1@hotmail.co.uk>
2024-05-22 08:10:02 +0100

    fix: update lint test

--- tests/testthat/test-hook-lintr.R
+++ tests/testthat/test-hook-lintr.R
@@ -1,3 +1,5 @@
+# .R ----
+
 # success
 run_test("lintr",
   suffix = "-success.R",
@@ -12,3 +14,11 @@ run_test(
   "lintr",
   suffix = "-fail.R", cmd_args = "--warn_only", std_err = NULL
 )
+
+# .qmd ----
+
+# success
+run_test("lintr", suffix = "-success.qmd", std_err = NULL)
+
+# failure
+run_test("lintr", suffix = "-fail.qmd", std_err = "not lint free")
