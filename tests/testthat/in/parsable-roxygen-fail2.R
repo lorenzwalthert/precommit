1bf87accf5e8c0896afb33a068e7bb12df96a194
owenjonesuob <21007837+owenjonesuob@users.noreply.github.com>
2024-06-21 11:54:14 +0100

    Minimalise roxygen comment

--- tests/testthat/in/parsable-roxygen-fail2.R
+++ tests/testthat/in/parsable-roxygen-fail2.R
@@ -1,16 +1,4 @@
-#' Some function
-#' 
-#' This function is great! But \code{oh dear, a missing brace...
-#' 
-#' And isn't that a multi-line example? We should probably use @examples...
-#' 
-#' @param x A parameter.
-#'
-#' @returns Invisible `NULL`.
-#' 
-#' @example
-#' some_function(10)
-#' some_function(11)
+#' Minimal roxygen docs, but there's a problem with our R code!
 #' 
 #' @export
 some_function <- function(x) {
