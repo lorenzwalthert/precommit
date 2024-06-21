4043ada9afd4d39c9a5e0afa6dd03fdbd0dd37b4
owenjonesuob <21007837+owenjonesuob@users.noreply.github.com>
2024-06-21 12:47:54 +0100

    Check for messages from roxygen2, rather than warnings

--- inst/hooks/exported/parsable-roxygen.R
+++ inst/hooks/exported/parsable-roxygen.R
@@ -2,15 +2,23 @@
 files <- commandArgs(trailing = TRUE)
 
 out <- lapply(files, function(path) {
+  
   tryCatch(
-    roxygen2::parse_file(path, env = NULL),
-    warning = function(w) {
-      cat(c("Roxygen commentary in file ", path, " is not parsable. Full context:\n"))
-      stop(conditionMessage(w), call. = FALSE)
-    },
+    # Capture any messages from roxygen2:::warn_roxy()
+    msg <- capture.output(
+      roxygen2::parse_file(path, env = NULL),
+      type = "message"
+    ),
+    
+    # In case we encounter a more general file parsing problem
     error = function(e) {
       cat(c("File ", path, " is not parsable. Full context:\n"))
       stop(conditionMessage(e), call. = FALSE)
     }
   )
+  
+  if (length(msg) > 0) {
+    cat(c("Roxygen commentary in file ", path, " is not parsable. Full context:\n"))
+    stop(paste0(msg, collapse = "\n"), call. = FALSE)
+  }
 })
