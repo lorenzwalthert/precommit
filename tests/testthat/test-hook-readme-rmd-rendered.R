68ef20679f9d88d5361c98cd527926b6e5a1827b
Jesse C. Ross <jross@usgs.gov>
2026-06-15 11:17:30 -0700

    add qmd tests for readme-rmd-rendered

--- tests/testthat/test-hook-readme-rmd-rendered.R
+++ tests/testthat/test-hook-readme-rmd-rendered.R
@@ -62,4 +62,55 @@ if (has_git()) {
       }
     )
   }
+
+  # qmd: out of date
+  run_test("readme-rmd-rendered",
+    file_name = c("README.md", "README.qmd"),
+    suffix = "",
+    std_err = "out of date",
+    std_out = NULL,
+    file_transformer = function(files) {
+      if (length(files) > 1) {
+        content_2 <- readLines(files[2])
+        Sys.sleep(2)
+        writeLines(content_2, files[2])
+        git_init()
+        git2r::add(path = files)
+      }
+      files
+    }
+  )
+
+  # only has qmd
+  run_test("readme-rmd-rendered",
+    file_name = "README.qmd",
+    suffix = "",
+    std_err = NULL,
+    std_out = NULL,
+    file_transformer = function(files) {
+      git_init()
+      git2r::add(path = files[1])
+      files
+    }
+  )
+
+  if (!on_windows_on_cran()) {
+    # qmd: only one file staged
+    run_test("readme-rmd-rendered",
+      file_name = c("README.qmd", "README.md"),
+      suffix = "",
+      std_err = "should be both staged",
+      std_out = NULL,
+      file_transformer = function(files) {
+        if (length(files) > 1) {
+          content_2 <- readLines(files[2])
+          Sys.sleep(2)
+          writeLines(content_2, files[2])
+          git_init()
+          git2r::add(path = files[1])
+        }
+        files
+      }
+    )
+  }
 }
